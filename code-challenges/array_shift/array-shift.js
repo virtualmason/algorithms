40483e3afb78a54339cc71342dca04af13eece8a
Floyd Orr <floydcanhelp@gmail.com>
2019-05-21 23:23:14 -0500

    one case passing

--- code-challenges/array_shift/array-shift.js
+++ code-challenges/array_shift/array-shift.js
@@ -1,12 +1,29 @@
 
-// Write a function called insertShiftArray which takes in an array and the value to be added. Without utilizing any of the built-in methods available to your language, return an array with the new value added at the middle index.
-// Example
-// Input	Output
-// [2,4,6,8], 5	[2,4,5,6,8]
-// [4,8,15,23,42], 16	[4,8,15,16,23,42]
-// Stretch Goal
-// Once you’ve achieved a working solution, write a second function that removes an element from the middle index and shifts other elements in the array to fill the new gap.
 
+
+// const insertShiftArray = (arr, num) => {
+//   let result =0;
+//   let result2 = []
+//   let empty = []
+//   let arr2 = [];
+//   for(let i = 0; i < arr.length /2; i++) {
+//     arr2[i] = arr[i]
+//     result = arr2[i+1] =num;
+//     arr2[i+1]
+//   }
+//  for (let j = Math.floor(arr.length); j > arr.length /2; j--) {
+//    if(typeof result2[j-2] === 'undefined' ) {
+
+//    } else{
+//     result2[j-2] = arr[j]
+
+
+//    }
+//  }
+//  console.log(...arr2, ...result2)
+//  empty = [...arr2, ...result2];
+//  return empty;
+// }
 const insertShiftArray = (arr, num) => {
   let result =0;
   let result2 = []
@@ -16,7 +33,6 @@ const insertShiftArray = (arr, num) => {
     arr2[i] = arr[i]
     result = arr2[i+1] =num;
     arr2[i+1]
-    
   }
  for (let j = arr.length/2+1; j > 0; j--) {
     result2[j-2] = arr[j]
@@ -24,5 +40,5 @@ const insertShiftArray = (arr, num) => {
  empty = [...arr2, ...result2];
  return empty;
 }
-module.exports = insertShiftArray;
 
+module.exports = insertShiftArray;
--- code-challenges/array_shift/array-shift.test.js
+++ code-challenges/array_shift/array-shift.test.js
@@ -1,12 +1,15 @@
 let array = require('./array-shift');
-// console.log(test([2,4,6,8], 5))
-// console.log(test([2,4,6,8], 5))
-describe('SHould insert in middle', () => {
-  test('It insert 5 in middle of 4 numbers', () => {
+
+describe('Should insert in middle', () => {
+  it('It insert 5 in middle of 4 numbers', () => {
     expect(array([2,4,6,8], 5)).toStrictEqual([2,4,5,6,8]);
   });
 });
-
+// describe('Should insert in middle', () => {
+//   it('It insert 5 in middle of 6 numbers', () => {
+//     expect(array([2,4,6,8,10,12], 5)).toStrictEqual([2,4,6,5,8,10,12]);
+//   });
+// });
 // insertShiftArray([2,4,6,8], 5);
 // insertShiftArray([2,4,6,8,10], 5);
 // insertShiftArray([2,4,6,8,10,12], 5);
