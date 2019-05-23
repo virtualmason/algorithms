3d61bc40fe45e1622df7d622a7be7ecbd0661646
Floyd Orr <floydcanhelp@gmail.com>
2019-05-22 22:02:00 -0500

    add better error handle in fs.js

---
+++ code-challenges/array_binary_search/array-binary-search.test.js
@@ -0,0 +1,13 @@
+let func = require('./array-binary-search');
+
+
+describe('binary function', () => {
+  it('return key if there', () => {
+    expect(func([1,2,3,4,5,6,7,8,12], 6)).toBe(4)
+    
+    });
+    it('return -1 if not found', () => {
+      expect(func([1,2,3,4,5,6,7,8,12]), 9).toBe(-1)
+      
+      });
+  });
